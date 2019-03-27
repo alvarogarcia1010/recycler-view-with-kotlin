aa4e9236ef46bc64b05d667e9b92999de7a7ac45
Alvaro Francisco García <00111317@uca.edu.sv>
2019-03-27 08:42:34 -0600

    Agregando NetworkUtil

--- app/src/main/java/com/agarcia/recyclerviewexample/PokemonAdapter.kt
+++ app/src/main/java/com/agarcia/recyclerviewexample/PokemonAdapter.kt
@@ -1,2 +1,34 @@
 package com.agarcia.recyclerviewexample
 
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.agarcia.recyclerviewexample.Models.Pokemon
+import kotlinx.android.synthetic.main.card_pokemon.view.*
+
+class PokemonAdapter(val pokemons: List<Pokemon>): RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_pokemon, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return pokemons.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(pokemons[position])
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(item: Pokemon) = with(itemView) {
+            tv_poke_name.text = item.name
+            tv_poke_type.text = item.type
+        }
+    }
+
+}
